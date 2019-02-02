b543d04415fd3c5e33c3f262a02f259946b3ce19
DIMITRY BESSON <DIMITRY.BESSON@cbc.ca>
2019-02-02 15:18:05 -0500

    Add french sports top stories endpoint

--- controllers/frControllers/frController.js
+++ controllers/frControllers/frController.js
@@ -1,8 +1,10 @@
 const express = require('express')
 const newsController = require('./newsController')
+const sportsController = require('./sportsController')
 
 const router = express.Router()
 
 router.use('/news', newsController)
+router.use('/sports', sportsController)
 
 module.exports = router
---
+++ controllers/frControllers/sportsController.js
@@ -0,0 +1,48 @@
+const express = require('express')
+const request = require('request-promise-native')
+
+const router = express.Router()
+
+const baseUrl = 'https://services.radio-canada.ca/hackathon/neuro/v1'
+const key = '31b2bb0e-85ec-4406-9b22-31c93d7e75f9'
+const keyQuery = `?client_key=${key}`
+
+const mapNeuroThemeItem = (item) => {
+  // some item objects don't have summaryImage
+  console.log(item.id)
+  console.log(item)
+
+  const imageUrl = item.summaryMultimediaItem && item.summaryMultimediaItem.concreteImages ? item.summaryMultimediaItem.concreteImages[0].mediaLink.href : ''
+
+  return {
+    id: item.id,
+    title: item.title,
+    source: 'neuro',
+    sourceId: item.id,
+    image: imageUrl,
+    publishedAt: null,
+    readablePublishedAt: null,
+    updatedAt: item.publishedLastTimeAt,
+    readableUpdatedAt: null,
+    numViewers: null
+  }
+}
+
+const getSportsContent = path => {
+
+  return request({
+    uri: `${baseUrl}${path}${keyQuery}`,
+    json: true
+  })
+
+  .then(data => data.pagedList.items.map(mapNeuroThemeItem))
+}
+
+router.get('/top-stories', (req, res) => {
+
+  getSportsContent('/themes/6/lineup')
+
+  .then(data => res.send(data))
+})
+
+module.exports = router
