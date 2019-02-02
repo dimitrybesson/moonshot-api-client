22b1d84256ba8d030551f17f8a78160d322f5621
DIMITRY BESSON <DIMITRY.BESSON@cbc.ca>
2019-02-01 21:48:24 -0500

    Add getters for first 3 intents

--- controllers/enControllers/newsController.js
+++ controllers/enControllers/newsController.js
@@ -3,6 +3,50 @@ const request = require('request-promise-native')
 
 const router = express.Router()
 
-router.get('/top-stories', (req, res) => res.send('English top stories'))
+const mapAggrNewsItem = (item) => {
+  return {
+    id: item.id,
+    title: item.title,
+    image: item.typeAttributes.imageLarge,
+    publishedAt: item.publishedAt,
+    readablePublishedAt: item.readablePublishedAt,
+    updatedAt: item.updatedAt,
+    readabUpdatedAt: item.readabUpdatedAt,
+    numViewers: item.typeAttributes.trending.numViewers,
+    url: item.typeAttributes.url
+  }
+}
+
+const baseUrl = 'https://api-gw.radio-canada.ca/aggregate-content/v1'
+
+const getNewsContent = (path) => {
+  return request({
+    uri: `${baseUrl}${path}`,
+    json: true
+  })
+
+  .then(data => data.map(mapAggrNewsItem))
+}
+
+router.get('/top-stories', (req, res) => {
+
+  getNewsContent('/items?lineupSlug=trending-news')
+
+  .then(data => res.send(data))
+})
+
+router.get('/world', (req, res) => {
+
+  getNewsContent('/categories/36/items')
+
+  .then(data => res.send(data))
+})
+
+router.get('/local', (req, res) => {
+
+  getNewsContent('/categories/55/items')
+
+  .then(data => res.send(data))
+})
 
 module.exports = router
