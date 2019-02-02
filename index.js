c827834fc205ae2c94d0f7d1992c9b00bcadbbb0
DIMITRY BESSON <DIMITRY.BESSON@cbc.ca>
2019-02-01 19:57:20 -0500

    Add express server

---
+++ index.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => res.send('Welcome to the root route'))
+
+app.listen(port, () => console.log(`Server is listening on port ${port}`))
