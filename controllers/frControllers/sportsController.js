f7bd94319fe83967a64fdbed3d2b8686e3b5a23b
DIMITRY BESSON <DIMITRY.BESSON@cbc.ca>
2019-02-03 01:55:50 -0500

    Updated neuro api to prod version

--- controllers/frControllers/sportsController.js
+++ controllers/frControllers/sportsController.js
@@ -3,14 +3,14 @@ const request = require('request-promise-native')
 
 const router = express.Router()
 
-const baseUrl = 'https://services.radio-canada.ca/hackathon/neuro/v1'
+const baseUrl = 'https://services.radio-canada.ca/neuro/v1'
 const key = '31b2bb0e-85ec-4406-9b22-31c93d7e75f9'
 const keyQuery = `?client_key=${key}`
 
 const mapNeuroThemeItem = (item) => {
   // some item objects don't have summaryMultimediaItem
 
-  const imageUrl = item.summaryMultimediaItem && item.summaryMultimediaItem.concreteImages ? item.summaryMultimediaItem.concreteImages[0].mediaLink.href : ''
+  const imageUrl = item.summaryImage && item.summaryImage.concreteImages ? item.summaryImage.concreteImages[0].mediaLink.href : ''
 
   return {
     id: item.id,
